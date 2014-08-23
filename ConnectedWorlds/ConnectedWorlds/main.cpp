e48c812643a7c211a9a42e205ca126a514adc80f
Xenetics <xenetics@live.com>
2014-08-23 03:45:07 -0400

    OOps

--- ConnectedWorlds/ConnectedWorlds/main.cpp
+++ ConnectedWorlds/ConnectedWorlds/main.cpp
@@ -11,16 +11,6 @@ int main(int argc, char *argv[])
 	game->Run();
 	game->Clean();
 
-	SDL_Surface* Surf_Display;
-
-	SDL_Window *sdlWindow;
-	SDL_Renderer *sdlRenderer;
-	SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_SHOWN, &sdlWindow, &sdlRenderer);
-
-	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(sdlRenderer);
-	SDL_RenderPresent(sdlRenderer);
-
 	SDL_Quit();
 	return 0;
 }
