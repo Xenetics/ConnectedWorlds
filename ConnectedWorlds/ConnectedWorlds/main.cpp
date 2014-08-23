d52bf13af37e4cce68f49b24fc4596b22a6a41e0
AlienJim <burton.alexander102@gmail.com>
2014-08-23 02:50:49 -0400

    Fixed Full Screen

--- ConnectedWorlds/ConnectedWorlds/main.cpp
+++ ConnectedWorlds/ConnectedWorlds/main.cpp
@@ -8,12 +8,14 @@ int main(int argc, char *argv[])
 
 	SDL_Window *sdlWindow;
 	SDL_Renderer *sdlRenderer;
-	SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP, &sdlWindow, &sdlRenderer);
+	SDL_CreateWindowAndRenderer(1280, 720, SDL_WINDOW_SHOWN, &sdlWindow, &sdlRenderer);
 
 	SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
 	SDL_RenderClear(sdlRenderer);
 	SDL_RenderPresent(sdlRenderer);
 
+	for (;;){}
+	
 
 	SDL_Quit();
 	return 0;
