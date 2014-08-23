df3a92611021b39b3ecc648b9f6eb9256ef72e45
Xenetics <xenetics@live.com>
2014-08-23 12:59:52 -0400

    Sprite class Done
    to be tested

--- ConnectedWorlds/ConnectedWorlds/Sprite.cpp
+++ ConnectedWorlds/ConnectedWorlds/Sprite.cpp
@@ -1,13 +1,14 @@
 #include "Sprite.h"
+#include "SDL.h"
 
 
-Sprite::Sprite(SDL_Surface* pSurfaceToDrawOn)
+void Sprite::Draw(SDL_Surface* pSurfaceToDrawOn, const b2Vec2& cameraPos)
 {
 	//maybe we dont need this SDL_RLEACCEL 
-	//SDL_SetAlpha(pImage_, SDL_SRCALPHA | SDL_RLEACCEL, alpha);
+	SDL_SetSurfaceAlphaMod(pImage_,  alpha);
 	SDL_Rect destR;
-	//destR.x = (Sint16)pos_.x - (Sint16)(pImage_->clip_rect.w / 2);
-	//destR.y = (Sint16)pos_.y - (Sint16)(pImage_->clip_rect.h / 2);
+	destR.x = (Sint16)pos_.x - (Sint16)(pImage_->clip_rect.w / 2);
+	destR.y = (Sint16)pos_.y - (Sint16)(pImage_->clip_rect.h / 2);
 
 	SDL_BlitSurface(pImage_, NULL, pSurfaceToDrawOn, &destR);
 }
--- ConnectedWorlds/ConnectedWorlds/Sprite.h
+++ ConnectedWorlds/ConnectedWorlds/Sprite.h
@@ -2,24 +2,48 @@
 
 #include "SDL.h"
 #include "SDL_image.h"
+#include <Box2D/Box2D.h>
+#include <Box2D/Common/B2Math.h>
 
 class Sprite
 {
 protected:
 	SDL_Surface* pImage_;
 
+	b2Vec2 pos_;
 	Uint8 alpha;
 public:
 	Sprite() : pImage_(0)
 	{}
-	Sprite(SDL_Surface* image, Uint8 alpha) : pImage_(image), alpha(alpha)
+	Sprite(SDL_Surface* image, b2Vec2 pos, Uint8 alpha) : pImage_(image), alpha(alpha)
 	{}
+	virtual void Draw(SDL_Surface* surfaceToDrawOn, const b2Vec2& cameraPosz);
+
 	virtual ~Sprite()
 	{
 		if (pImage_)
 		{
 			SDL_FreeSurface(pImage_);
 		}
+	}
+
+	//accessors
+	b2Vec2 getPos()
+	{
+		return pos_;
+	}
+	void setPos(b2Vec2 pos)
+	{
+		pos_ = pos;
+	}
+	Uint8 getAlpha()
+	{
+		return alpha;
+	}
+
+	void setAlpha(Uint8 alpha)
+	{
+		alpha = alpha;
 	}
 };
 
